473fbd78b74383d8611448c16f99496d4bd791df
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-09-21 13:35:01 +0300

    Clear the code for better reading.

--- src/simplegui3/simplegui3.java
+++ src/simplegui3/simplegui3.java
@@ -4,32 +4,26 @@ import java.awt.event.*;
 public class simplegui3 implements ActionListener  {
     JButton button = null;
     private int numClick = 0;
-    
+
     public static void main(String[] args) {
         simplegui3 gui = new simplegui3();
         gui.go();
-
     }
 
     public void go() {
         JFrame frame = new JFrame();
         button = new JButton("Click me.");
-        
         button.addActionListener(this);
-        
         frame.getContentPane().add(button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
-        
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        button.setLocation(100, 100); //This code do not change the button location if numClick++ (next row) used. 
-                                      //If comment numClick++ the button changes location on click. Why?
-        numClick++;
+        button.setLocation(100, 100); //This code do not change the button location if numClick++ (next row) used.               
+        numClick++;                   //If comment numClick++ the button changes location on click. Why location change if this row uncomment?
         button.setText("Has been clicked " + numClick + " times.");
-        
     }
 }
