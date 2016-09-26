c61f89563f4e511264b9bc91c2ec40735e15e438
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-09-26 15:40:09 +0300

    File rename. Start with app structure.

---
+++ Calculator/src/calculator/CalculatorRunner.java
@@ -0,0 +1,9 @@
+package calculator;
+ //@date Sep 26, 2016
+ //@author Artsiom_Berazavik
+
+public class CalculatorRunner {
+    public static void main(String[] args) {
+        
+    }
+}
--- Calculator/src/calculator/Concationation.java
+++ Calculator/src/calculator/Concationation.java
@@ -1,6 +1,6 @@
 //@Date Sep 26, 2016
 //@author Artsiom_Berazavik
 
-public class Calculator {
-//test
+public class Concationation {
+
 }
