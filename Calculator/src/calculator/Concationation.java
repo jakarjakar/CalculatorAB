c2853fd52405cf44b493726ae06317bdf8df5686
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-10-11 18:21:10 +0300

    Added requirement i main class in comments

--- Calculator/src/calculator/Concationation.java
+++ Calculator/src/calculator/Concationation.java
@@ -1,6 +1,19 @@
+package calculator;
 //@Date Sep 26, 2016
 //@author Artsiom_Berazavik
 
 public class Concationation {
+    
+//Constructor, wait for two Intager.
+    public Concationation (int a, int b) {
+    }
+    
+    public Concationation () {
+        //создать конструктор вызываемый с пустыми параметрами и переадресовать создание конструкторы выше через this
+    }
+    
+    public int add (int a, int b) {
+        return a + b;
+    }
 
 }
