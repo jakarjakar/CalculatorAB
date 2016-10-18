fae8cd6d049752a9a609198f241a4684a1fde004
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-10-18 18:49:28 +0300

    Matrix multiplicatoin v 1.1. Logic improved. work with not square matrix too.

--- MatrixMultiplication/src/matrixmultiplication/Mult.java
+++ MatrixMultiplication/src/matrixmultiplication/Mult.java
@@ -4,7 +4,7 @@ package matrixmultiplication;
 import java.util.Arrays;
 
 public class Mult {
-    public static int [][] result (int m0[][], int m1[][] ) {
+    public static double [][] result (double m0[][], double m1[][] ) {
         
         
         int length_m0_row = m0.length;
@@ -12,13 +12,22 @@ public class Mult {
         
 //        int length_m1_row = m1.length;
         int length_m1_column = m1[0].length;
-        int [][] m2 = new int[length_m0_row][length_m1_column];
+//        double [][] m2 = new double[length_m0_row][length_m1_column];
+//        Mtrx.print(m2);
+        
+        double[][] m2 = new double[length_m0_row][length_m1_column];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                m2[i][j] = 0.00000;
+            }
+        }
         
         for (int i = 0; i < length_m0_row; i++) {
             for (int j = 0; j < length_m1_column; j++) {
                 for (int k = 0; k < length_m0_column; k++) {
-                    m2[i][j] = (m0[i][j] * m1[i][j]) + (m0[i][k] * m1[k][j]);
-                    System.out.println("Hi" + Arrays.deepToString(m2));
+//                    m2[i][j] = (m0[i][j] * m1[i][j]) + (m0[i][k] * m1[k][j]); Не работает с не квадратными матрицами, фак!
+                    m2[i][j] += m0[i][k] * m1[k][j];
+//                    System.out.println("Hi" + Arrays.deepToString(m2));
                 }
             }
         } 
