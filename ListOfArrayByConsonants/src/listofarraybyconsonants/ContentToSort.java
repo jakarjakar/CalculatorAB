1c0018c4770abc491347e235f6a82c30f0d2e24d
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-10-28 15:04:05 +0300

    Added comments.

--- ListOfArrayByConsonants/src/listofarraybyconsonants/ContentToSort.java
+++ ListOfArrayByConsonants/src/listofarraybyconsonants/ContentToSort.java
@@ -1,25 +1,23 @@
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import javax.print.DocFlavor;
 
 //@Date Oct 25, 2016
 //@author Artsiom_Berazavik
 
 public class ContentToSort {
     public static void main(String[] args) {
-    //Add 3 sentence fo test. 
-        String str1H = "Here is a next string to compare";
+    //Add 3 sentence fo test 
+        String str1H = "Here is a next string to compare.";
         String str2H = "Here is a string number two.";
-        String str3H = "Here is a string.";
+        String str3H = "Here is a next string to comparerr.";
         
-    //lovercase sentences
+    //Lovercase sentences
         String str1 = str1H.toLowerCase();
         String str2 = str2H.toLowerCase();
         String str3 = str3H.toLowerCase();
         
-    //Prepare arrays from the sentence characters.
+    //Prepare arrays from the sentence characters
         char[] str1ToChar = str1.toCharArray(); 
         char[] str2ToChar = str2.toCharArray();
         char[] str3ToChar = str3.toCharArray();
@@ -29,8 +27,7 @@ public class ContentToSort {
         for(char i : str1ToChar) {
             arrList1.add(i);
         }
-        System.out.println(arrList1);
-        
+
         List<Character> arrList2 = new ArrayList<>();
         for(char i : str2ToChar) {
             arrList2.add(i);
@@ -40,82 +37,62 @@ public class ContentToSort {
         for(char i : str3ToChar) {
             arrList3.add(i);
         }
-        
-//    //Aff all ArraLists in resultUnRated ArrayList
-//        ArrayList resultUnRated = new ArrayList();
-//        resultUnRated.add(arrList1);
-//        resultUnRated.add(arrList2);
-//        resultUnRated.add(arrList3);
                 
     //Prepare Array List of all English consonants.
         List<Character> consonants = new ArrayList<>();
         for (char i : "wrtpsdfghjklzxcvbnm".toCharArray()) {
             consonants.add(i);
         }
-    
-//    //Prepare ArrayList of all English vowels.
-//        ArrayList vowels = new ArrayList();
-//        for (char i : "eyuioa".toCharArray()) {
-//            vowels.add(i);
-//        }
-        
-        int nConc = 0;
-        
+    //Fill rateList by null's to avoid exception when add non onbject by index
         ArrayList<Object> ratedList = new ArrayList<Object>();
         for (int i = 0; i < 999; i++) {
             ratedList.add(i, null);
         }        
-        
-        System.out.println("First printed most /consonants/ sentences");
-        
+
+    /*For loops will be moved to separate method and class. This "code" is just Prototype.
+    Inside two loops there is comparing every letter in sentence with the consonants
+    When leеtter in the sentence equal of any consonant counter++
+    */
+        int n1Conc = 0;
         for (int i = 0; i < arrList1.size(); i++) {
             for (int j = 0; j < consonants.size(); j++) {
                 if (Objects.equals(arrList1.get(i), consonants.get(j))) {
                     //IDE changed arrList1.get(i) == consonants.get(j) 
-                    nConc++;
+                    n1Conc++;
                 } //end of if
             } //end of consonant for
         } //end of arrList1 for
-        ratedList.add(nConc, str1H);
+        ratedList.add(n1Conc, str1H);
         
+        int n2Conc = 0;
         for (int i = 0; i < arrList2.size(); i++) {
             for (int j = 0; j < consonants.size(); j++) {
                 if (Objects.equals(arrList1.get(i), consonants.get(j))) {
                     //IDE changed arrList1.get(i) == consonants.get(j) 
-                    nConc++;
+                    n2Conc++;
                 } //end of if
             } //end of consonant for
-        } //end of arrList1 for
-        ratedList.add(nConc, str2H);
-        
+        } //end of arrList2 for
+        ratedList.add(n2Conc, str2H);
+
+        int n3Conc = 0;
         for (int i = 0; i < arrList3.size(); i++) {
             for (int j = 0; j < consonants.size(); j++) {
                 if (Objects.equals(arrList3.get(i), consonants.get(j))) {
                     //IDE changed arrList1.get(i) == consonants.get(j) 
-                    nConc++;
+                    n3Conc++;
                 } //end of if
             } //end of consonant for
-        } //end of arrList1 for
-        ratedList.add(nConc, str3H);
-        
-        
-        
+        } //end of arrList3 for
+        ratedList.add(n3Conc, str3H);
+
+        //Print sorted list.
+        System.out.println("First printed less /consonants/ sentences:");
         for (int i = 0; i < 999; i++) {
             if (ratedList.get(i) !=null) {
                 System.out.println(ratedList.get(i));
             }
-            
         }
-
-        
-
-        
-
-
-
-        
- 
     }
-    
 }
 
